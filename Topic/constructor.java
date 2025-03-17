e46392fa641ff40304ac9ee3efb90215e31f9ee3
Mohammad Rahat Hossan <131850941+codingwithrahat@users.noreply.github.com>
2025-03-18 01:16:30 +0600

    Create constructor.java

---
+++ Topic/constructor.java
@@ -0,0 +1,35 @@
+//package com.mycompany.oop;
+
+class Teacher {
+    String name;
+    String gender;
+    int phn;
+    
+    
+    Teacher(String n, String g, int p){
+        name = n;
+        gender = g;
+        phn = p;
+    }
+    
+    void displayInfo(){
+        System.out.println("Name : " + name);
+        System.out.println("Gender : " + gender);
+        System.out.println("Phn : " + phn);
+        
+    }
+    
+    
+}
+
+public class Oop {
+
+    public static void main(String[] args) {
+        Teacher t1 = new Teacher("Rahat", "male", 18);
+        Teacher t2 = new Teacher("Rakib", "male", 13);
+        
+        t1.displayInfo();
+        t2.displayInfo();
+        
+    }
+}
