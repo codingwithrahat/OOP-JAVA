b1d082e0211bb854be53ea717b8cded080dd31fa
Mohammad Rahat Hossan <131850941+codingwithrahat@users.noreply.github.com>
2025-04-08 01:59:26 +0600

    Create static_block_p3.java

---
+++ Topic/static_block_p3.java
@@ -0,0 +1,15 @@
+//package com.mycompany.oop;
+
+public class Oop {
+
+    
+    static{
+        System.out.println("Static Block");
+    }
+    
+    public static void main(String[] args) {
+        
+        System.out.println("Main Method");
+        //static block exicute before main method
+    }
+}
