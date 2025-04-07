520d3f6381fb3cc0332c01dcf3f8d6a2b184d9cd
Mohammad Rahat Hossan <131850941+codingwithrahat@users.noreply.github.com>
2025-04-08 01:01:17 +0600

    Create static_variable_p2.java

---
+++ Topic/static_variable_p2.java
@@ -0,0 +1,21 @@
+//package com.mycompany.oop;
+
+class Teacher {
+    
+    static String uniName = "SEU";
+  
+    
+}
+
+public class Oop {
+
+    public static void main(String[] args) {
+        //Teacher t1 = new Teacher();
+        //no need
+        
+        System.out.println("Uni Name: " + Teacher.uniName);
+        //direct call from class , Teacher.uniname
+        
+        
+    }
+}
