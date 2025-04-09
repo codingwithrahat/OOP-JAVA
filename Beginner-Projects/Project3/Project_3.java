946e7e34899fa50789c25607b0a3ef2dead3e670
Mohammad Rahat Hossan <131850941+codingwithrahat@users.noreply.github.com>
2025-04-10 00:28:42 +0600

    Create Project_3.java

---
+++ Beginner-Projects/Project3/Project_3.java
@@ -0,0 +1,30 @@
+package com.mycompany.project_3;
+
+class Box{
+    double height, width, depth;
+    
+    
+    Box(double height, double width, double depth){
+        this.height = height;
+        this.width = width;
+        this.depth = depth;
+    }
+    
+    void displayVol(){
+        double vol = height * width * depth;
+        System.out.println("Volume : " + vol);
+        
+    }
+    
+    
+}
+public class Project_3 {
+
+    public static void main(String[] args) {
+        Box b1 = new Box(10, 10, 10);
+        Box b2 = new Box(20, 30, 10);
+        
+        b1.displayVol();
+        b2.displayVol();
+    }
+}
