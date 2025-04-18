d6e7a128731ac2502837d96dff98d8237342f6c8
Mohammad Rahat Hossan <131850941+codingwithrahat@users.noreply.github.com>
2025-04-18 18:07:42 +0600

    Create this_p2.java

---
+++ Topic/this_p2.java
@@ -0,0 +1,38 @@
+//package com.mycompany.oop;
+
+class Teacher {
+    String name;
+    String gender;
+    int phn;
+    
+    Teacher(String name, String gender){
+        this.name = name;
+        this.gender = gender;
+    }
+    
+    Teacher(String name, String gender, int phn){
+        this(name, gender);
+        this.phn = phn;
+    }
+    
+    void displayInfo(){
+        System.out.println("Name : " + name);
+        System.out.println("Gender : " + gender);
+        System.out.println("Phn : " + phn);
+        
+    }
+    
+    
+}
+
+public class Oop {
+
+    public static void main(String[] args) {
+        Teacher t1 = new Teacher("Rahat", "male", 18);
+        Teacher t2 = new Teacher("Rakib", "male");
+        
+        t1.displayInfo();
+        t2.displayInfo();
+        
+    }
+}
