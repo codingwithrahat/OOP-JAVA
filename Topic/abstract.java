7e428ddf102b5321733ff78b6f89c9e30eff4097
Mohammad Rahat Hossan <131850941+codingwithrahat@users.noreply.github.com>
2025-04-18 21:26:29 +0600

    Create abstract.java

---
+++ Topic/abstract.java
@@ -0,0 +1,38 @@
+//package com.mycompany.oop;
+
+abstract class mobileUser{
+    abstract void sendMssg();
+}
+
+class rahat extends mobileUser{
+    // must create a override method
+    
+    @Override
+    void sendMssg(){
+        System.out.println("Hi, This is Rahat");
+    }
+}
+
+class rakib extends mobileUser{
+    // must create a override method
+    
+    @Override
+    void sendMssg(){
+        System.out.println("Hi, This is Rakib");
+    }
+}
+
+public class Oop {
+
+    public static void main(String[] args) {
+        mobileUser m;
+        //abstract class can create as a object
+        
+        m = new rahat();
+        m.sendMssg();
+        
+        m = new rakib();
+        m.sendMssg();
+
+    }
+}
