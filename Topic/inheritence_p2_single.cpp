bcb15a3d62a7c9324a16a3005793fc92deac6a69
Mohammad Rahat Hossan <131850941+codingwithrahat@users.noreply.github.com>
2025-05-19 01:32:38 +0600

    Create inheritence_p2_single.cpp

---
+++ Topic/inheritence_p2_single.cpp
@@ -0,0 +1,25 @@
+package com.mycompany.oop;
+
+//Single Inheritance
+
+class Animal {
+    void eat(){
+        System.out.println("Animal is eating");
+    }
+                
+}
+
+class Dog extends Animal{
+    void bark(){
+        System.out.println("Dog is barking");
+    }
+}
+
+public class Oop {
+
+    public static void main(String[] args) {
+        Dog d1 = new Dog();
+        d1.eat();
+        d1.bark();
+    }
+}
