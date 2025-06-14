24967a321492e83cdb9a2e4d6c7be365c238e62b
Mohammad Rahat Hossan <131850941+codingwithrahat@users.noreply.github.com>
2025-06-15 00:52:55 +0600

    Create thread_p1.java

---
+++ Topic/thread_p1.java
@@ -0,0 +1,10 @@
+//package com.mycompany.oop;
+
+public class Oop {
+
+    public static void main(String[] args) {
+        //main thread
+        System.out.println(1/0); //output - Exception in thread "main" java.....
+        
+    }
+}
