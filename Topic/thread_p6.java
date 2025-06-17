715677eb5108a64b00580cf19a18c9bffedb2b8c
Mohammad Rahat Hossan <131850941+codingwithrahat@users.noreply.github.com>
2025-06-17 21:08:13 +0600

    Create thread_p6.java

---
+++ Topic/thread_p6.java
@@ -0,0 +1,37 @@
+//package com.mycompany.oop;
+
+class A extends Thread{
+    
+    A(String s){
+        super(s); //it take a name and pass it to thread constructor
+    }
+
+    @Override
+    public void run(){
+        for(int i = 1; i<=5; i++){
+            System.out.println("Thread A : " + i);
+            try {
+                Thread.sleep(1000);
+            } catch (Exception e) {
+                System.out.println(e);
+            }
+        }
+    }
+}
+
+public class Oop {
+    
+    public static void main(String[] args){
+        
+        A t1 = new A("My Thread");
+        t1.start();
+
+        System.out.println("Name : " + t1.getName()); //to get thread name
+        
+        
+        Thread t2 = Thread.currentThread(); //currentThread return which thread is currently execute
+        
+        System.out.println("Name : " + t2.getName());  
+        
+    }
+}
