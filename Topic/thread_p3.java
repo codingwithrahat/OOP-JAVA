9a3cb07e3c13ed12edbf2daed4dfda084cbd844c
Mohammad Rahat Hossan <131850941+codingwithrahat@users.noreply.github.com>
2025-06-17 20:14:23 +0600

    Create thread_p3.java

---
+++ Topic/thread_p3.java
@@ -0,0 +1,39 @@
+//package com.mycompany.oop;
+
+class A extends Thread{
+
+    @Override
+    public void run(){
+        for(int i = 1; i<=5; i++){
+            System.out.println("Thread A : " + i);
+            try {
+                Thread.sleep(1000);
+            } catch (Exception e) {
+                System.out.println(e);
+            }
+        }
+    }
+}
+
+public class Oop {
+    
+    public static void main(String[] args){
+        
+        // main thread start here
+        A t1 = new A();
+       
+        t1.start(); //t1 thread will start
+        
+        for(int j = 1; j<=5; j++){
+            // mian thread is runing , But thread A is sleeping after each print
+            // so main thread finish its loop early
+            System.out.println("Thread Main : " + j);
+//            try {
+//                Thread.sleep(1000);
+//            } catch (Exception e) {
+//                System.out.println(e);
+//            }
+        }
+        
+    }
+}
