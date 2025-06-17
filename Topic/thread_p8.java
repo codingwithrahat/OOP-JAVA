762c14cc9c9565075c94cc5f93596d900ea8dcf2
Mohammad Rahat Hossan <131850941+codingwithrahat@users.noreply.github.com>
2025-06-17 21:27:07 +0600

    Create thread_p8.java

---
+++ Topic/thread_p8.java
@@ -0,0 +1,33 @@
+//package com.mycompany.oop;
+
+class A extends Thread{
+    
+    A(String s){
+        super(s); //it take a name and pass it to thread constructor
+    }
+    int x;
+    @Override
+    public void run(){
+        
+        for(int i = 1; i<=100000; i++){
+            x++;
+        }
+    }
+}
+
+public class Oop {
+    
+    public static void main(String[] args) throws InterruptedException{
+        
+        A t1 = new A("My Thread");
+        
+        System.out.println(t1.isAlive()); // dont throw any error
+        //if thread not started it return false
+        
+        t1.start();
+  
+        System.out.println(t1.isAlive()); //if thread started it return true
+
+        
+    }
+}
